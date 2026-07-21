0a381cd9a30e93a32a604b332846e184e326fbdc
Afrid-40 <sahilmrxofficial@gmail.com>
2026-07-21 10:24:17 +0530

    testcase2

--- excpetionhandle.py
+++ excpetionhandle.py
@@ -1,10 +1,19 @@
-x = int(input("Enter a number: "))
-y = int(input("Enter a number: "))
-try:
-    print(x/y)
-except ZeroDivisionError as e:
-    print(e)
-except ValueError as e:
-    print(e)
-finally:
+# x = int(input("Enter a number: "))
+# y = int(input("Enter a number: "))
+# try:
+#     print(x/y)
+# except ZeroDivisionError as e:
+#     print(e)
+# except ValueError as e:
+#     print(e)
+# finally:
+#     print("done")
+
+
+for i in range(5):
+    print(i)
+    if i==4:
+        break
+        print(i)
+else:
     print("done")
